6849509e4a7d6cfee28ebff03041eb42c694289f
Cody Schank <codyschank@gmail.com>
2020-06-14 20:31:26 -0500

    Crop first raster to study area

--- processing.R
+++ processing.R
@@ -1,1 +1,11 @@
 library(raster)
+library(rgdal)
+
+setwd('/Users/codyschank/kouprey/')
+
+TCC_2017 = raster('imagery/TCC_2017.tif')
+studyArea = readOGR('spatial_data/study_area.shp')
+
+TCC_2017_crop = crop(TCC_2017, studyArea)
+
+plot(TCC_2017_crop)
